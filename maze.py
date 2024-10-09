34c7fa0a7e26a58df34bdc1e397b84b0e3580834
Mkhabele Mikejunior Mark Mashaole <137772011+mkhmik004@users.noreply.github.com>
2024-10-09 02:49:41 +0200

    finished  maze.py
    prints out the maze

--- maze.py
+++ maze.py
@@ -1,46 +1,43 @@
 import random
-outerlist =[]
+
+outerlist = []
 walls = ['#', 'o']
 
 def first_row(n):
     inner_list = []
-    for row in range (n):
+    for row in range(n):
         x = random.choice(walls)
-        inner_list.append (x)
+        inner_list.append(x)
     return inner_list
 
-def maze(n,outer):
+def maze(n, outer):
     locationlist = []
     counter = 0
     top_list = outer[-1]
+    # Find the positions of "o" in the previous row
     for location in top_list:
         if location == "o":
-            locationlist.append (counter)
-            counter += 1
-        else:
-            counter += 1 
-            continue
-    inner_list = []
-    index = 0 
-    for i in range (n-1):
-        if index in locationlist:
-            position = random.choice([index -1, index, index+ 1])
-            while 0 <= position <= n-1:
-                 position = random.choice([index -1, index, index+ 1])
-            inner_list[position] = "o"
-            index+=1
-        else:
-            inner_list.append ('#')
-            index+=1
-    print(inner_list)
-    outerlist.append(inner_list)
+            locationlist.append(counter)
+        counter += 1
     
+    inner_list = ['#'] * n 
+    
+    for index in locationlist:
+        # Choose position for "o" to create path
+        position = random.choice([index - 1, index, index + 1])
+        # Ensure position is within bounds
+        position = max(0, min(position, n - 1))
+        inner_list[position] = "o"  # Place the path "o"
+    
+    outerlist.append(inner_list)
+
 def main():
-    size = input ("Enter length of maze: ")
-    outerlist.append(first_row(int(size)))
-    print (outerlist)
-    maze(int(size),outerlist)
+    size = int(input("Enter length of maze: "))  # Convert input to int
+    outerlist.append(first_row(size))
+    for _ in range(size - 1):
+        maze(size, outerlist)
+        
     print(outerlist)
 
-if  __name__ == '__main__':
-  main()
+if __name__ == '__main__':
+    main()
